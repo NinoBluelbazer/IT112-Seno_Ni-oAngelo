de0b62bee0cbdfe1fb872db14642e1ad983de63a
NinoBluelbazer <156326907+NinoBluelbazer@users.noreply.github.com>
2026-08-18 21:05:00 +0800

    Add welcome message program in welcomeconsole.c
    This file contains a C program that prints a decorative welcome message to the console.

---
+++ welcomeconsole.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main()
+{
+
+ printf("                             o o o o o             o o o o o ");
+    printf("      \n");
+ printf("                          o            o         o            o ");
+    printf("      \n");
+    printf("                        o                 o   o                 o");
+    printf("      \n");
+    printf("                         o                 o o                 o");
+    printf("      \n");
+    printf("                          o                 v                 o");
+    printf("      \n");
+    printf("                           o     WELCOME TO C PROGRAMMING    o");
+    printf("      \n");
+    printf("                            o                               o");
+    printf("      \n");
+    printf("                             o     Explore - Try - Work    o");
+    printf("      \n");
+    printf("                              o                           o");
+    printf("      \n");
+    printf("                               o                         o");
+    printf("      \n");
+    printf("                                o                       o");
+    printf("      \n");
+    printf("                                 o                     o");
+    printf("      \n");
+    printf("                                  o                   o");
+    printf("      \n");
+    printf("                                   o                 o");
+    printf("      \n");
+    printf("                                    o               o");
+    printf("      \n");
+    printf("                                     o             o");
+    printf("      \n");
+    printf("                                      o           o");
+    printf("      \n");
+    printf("                                       o         o");
+    printf("      \n");
+    printf("                                        o       o");
+    printf("      \n");
+    printf("                                         o     o");
+    printf("      \n");
+    printf("                                          o   o");
+    printf("      \n");
+    printf("                                           o o");
+    printf("      \n");
+    printf("                                            o ");
+
+
+
+    return 0;
+}
